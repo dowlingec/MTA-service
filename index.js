5f8589ae5d91abf1ddbf7e7272878c35a8148ebf
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-01 17:19:01 -0400

    station arrivals accesible

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const getAllStations = async () => {
     let res = await req.json()
     
     res.result.forEach((element) => {
-        // console.log('Stations', element)
         let h2 = document.createElement('h2')
         h2.innerText = element.name
+        h2.dataset.stationId = element.id
+        h2.addEventListener('click', () => {
+            getArrivalTimes(h2.dataset.stationId)
+        })
         div.append(h2)
     })
+}
+
+const getArrivalTimes = async (stationId) => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json()
+    console.log('Arrival Times', res.result.arrivals)
 }
 
 getAllStations()
